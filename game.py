9cdd164b4b7e668c7dd84441ccec904cca04302e
thejongu <jongu.gamer@gmx.de>
2021-03-20 18:34:44 +0100

    Fixed car physic with turning

--- game.py
+++ game.py
@@ -4,7 +4,13 @@ from math import sin, cos, atan2, radians, degrees, copysign
 from pygame.math import Vector2
 
 class Car:
-    def __init__(self, x, y):
+    def __init__(self, x, y, aMaxSpeed = 600, aBackSpeed = -100, aMinWheelbase = 50, aMaxWheelbase = 500):
+        # Constants
+        self.MAXFRONTSPEED = aMaxSpeed
+        self.MAXBACKSPEED = aBackSpeed
+        self.MINWHEELBASE = aMinWheelbase
+        self.MAXWHEELBASE = aMaxWheelbase
+
         self.position = Vector2(x, y)
 
         self.turningWheel = Vector2(0, 0)
@@ -31,47 +37,73 @@ class Car:
         else:
             self.friction()
 
+        theDeceleration = 0
+        if abs(self.steerAngle) > 1.6: theDeceleration += 2 
+        if abs(self.steerAngle) > 2.0: theDeceleration += 2
+        if abs(self.steerAngle) > 2.4: theDeceleration += 5
+        if abs(self.steerAngle) > 2.8: theDeceleration += 5
+        
+        if self.speed > 0: self.speed -= theDeceleration
+        else: self.speed += theDeceleration
+
+        if abs(self.speed) < 10: self.steerAngle = 0
+
         if aPressedKey[pygame.K_RIGHT]:
             self.steerRight()
         elif aPressedKey[pygame.K_LEFT]:
             self.steerLeft()
-       
-            
+        
+        if aPressedKey[pygame.K_j]:
+            self.position = (200,200)
+        
+        
+
         # Move the car
         self.update(aDelta)
 
     def accelerate(self):
-        self.speed += 2
-        if(self.speed > 600): self.speed = 600
+        self.speed += 3
+        if(self.speed > self.MAXFRONTSPEED): self.speed = self.MAXFRONTSPEED
     
     def decelerate(self):
         self.speed -= 8
-        if(self.speed < -100): self.speed = -100
+        if(self.speed < self.MAXBACKSPEED): self.speed = self.MAXBACKSPEED
     
     def friction(self):
-        if self.speed > 0: self.speed -= 5
-        else: self.speed += 5
-        if abs(self.speed < 10): self.speed = 0
+        theDeceleration = 5
+        if self.speed > 0: self.speed -= theDeceleration
+        else: self.speed += theDeceleration
+        if abs(self.speed < 20): self.speed = 0
 
     def steerRight(self):
+        theSteerAngle = -0.01
         if abs(self.speed) > 0:
-            self.steerAngle -= .05
-            if(abs(self.steerAngle) > 2): self.steerAngle = -2
+            if self.steerAngle > 0: theSteerAngle = -0.1 
+            elif self.steerAngle > -1.0: theSteerAngle = -0.060
+            elif self.steerAngle > -1.5: theSteerAngle = -0.025 
+            elif self.steerAngle > -3.0: theSteerAngle = -0.005
+            self.steerAngle += theSteerAngle
+            if abs(self.steerAngle) > 3: self.steerAngle = -3
         else:
             self.steerAngle = 0
     def steerLeft(self):
+        theSteerAngle = 0.01
         if abs(self.speed) > 0:
-            self.steerAngle += .05
-            if(abs(self.steerAngle) > 2): self.steerAngle = 2
+            if self.steerAngle < 0: theSteerAngle = 0.1
+            elif self.steerAngle < 1.0: theSteerAngle = 0.060 
+            elif self.steerAngle < 1.5: theSteerAngle = 0.025
+            elif self.steerAngle < 3.0: theSteerAngle = 0.005 
+            self.steerAngle += theSteerAngle
+            if(abs(self.steerAngle) > 3): self.steerAngle = 3
         else:
             self.steerAngle = 0
 
     def calcWheelBase(self):
         self.wheelBase = abs(self.speed)/2
-        if(self.wheelBase < 50):
-            self.wheelBase = 50
-        if(self.wheelBase > 300):
-            self.wheelBase = 300
+        if(self.wheelBase < self.MINWHEELBASE):
+            self.wheelBase = self.MINWHEELBASE
+        if(self.wheelBase > self.MAXWHEELBASE):
+            self.wheelBase = self.MAXWHEELBASE
         
             
 
@@ -112,7 +144,7 @@ class Game:
         current_dir = os.path.dirname(os.path.abspath(__file__))
         image_path = os.path.join(current_dir, "car.png")
         car_image = pygame.image.load(image_path)
-        car = Car(0, 0)
+        car = Car(200, 400)
         ppu = 32
 
         while not self.exit:
@@ -131,15 +163,19 @@ class Game:
             # Drawing
             self.screen.fill((50, 50, 50))
             
-            pygame.draw.circle(self.screen, (100, 100, 100), car.displayPos1, 15, 10)
-            pygame.draw.circle(self.screen, (100, 100, 100), car.displayPos2, 15, 10)
-            pygame.draw.circle(self.screen, (100, 100, 100), car.displayPos3, 15, 10)
-            pygame.draw.circle(self.screen, (100, 100, 100), car.displayPos4, 15, 10)
+            pygame.draw.circle(self.screen, (abs(car.steerAngle)*80, abs(car.steerAngle)*80, abs(car.steerAngle)*80), car.displayPos1, 15, 10)
+            pygame.draw.circle(self.screen, (abs(car.steerAngle)*80, abs(car.steerAngle)*80, abs(car.steerAngle)*80), car.displayPos2, 15, 10)
+            pygame.draw.circle(self.screen, (abs(car.steerAngle)*80, abs(car.steerAngle)*80, abs(car.steerAngle)*80), car.displayPos3, 15, 10)
+            pygame.draw.circle(self.screen, (abs(car.steerAngle)*80, abs(car.steerAngle)*80, abs(car.steerAngle)*80), car.displayPos4, 15, 10)
             pygame.draw.circle(self.screen, (0,255,0), car.displayPosFront, 5, 10)
             pygame.draw.circle(self.screen, (255,0,0), car.displayPosBack, 5, 10)
 
-            #pygame.draw.circle(self.screen, (100,255,0), car.frontWheel, 5, 10)
-            #pygame.draw.circle(self.screen, (255,100,0), car.turningWheel, 5, 10)
+            pygame.draw.circle(self.screen, (255, 255, 0), (300, 300), 15, 10)
+            pygame.draw.circle(self.screen, (255, 255, 0), (900, 300), 15, 10)
+           
+
+            pygame.draw.circle(self.screen, (100,255,0), car.frontWheel, 5, 10)
+            pygame.draw.circle(self.screen, (255,100,0), car.turningWheel, 5, 10)
 
 
             pygame.display.flip()
